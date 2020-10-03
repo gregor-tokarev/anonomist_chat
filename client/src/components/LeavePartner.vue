ed365f43bc7a63006ceb8fc8f0827b084d567433
zipper <zippper123915@gmail.com>
2020-10-03 12:23:09 +0300

    add build

---
+++ client/src/components/LeavePartner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container primary">
+    <h2>Partner Leave</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeavePartner'
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  text-align: center;
+}
+</style>
